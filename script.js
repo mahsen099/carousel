20a455cb7921566888ad7d701553511870c992bc
mahsen099 <mahsen099@gmail.com>
2021-10-17 21:36:50 -0500

    new update

--- script.js
+++ script.js
@@ -1,82 +1,118 @@
 $('.slider').slick({
-    dots: false,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    prevArrow: '<span class="prev_arrow"><i class="fas fa-angle-left"></i></span>',
-    nextArrow: '<span class="next_arrow"><i class="fas fa-angle-right"></i></span>',
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: false
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1.2,
-          slidesToScroll: 1
-        }
+  dots: false,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  prevArrow: '<span class="prev_arrow"><i class="fas fa-angle-left"></i></span>',
+  nextArrow: '<span class="next_arrow"><i class="fas fa-angle-right"></i></span>',
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: false
       }
-    ]
-  });
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1.2,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
 
 // vertical-comp
 $('.vertical-comp').slick({
-  dots: false,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  prevArrow: '<span class="prev_arrow"><i class="fas fa-angle-left"></i></span>',
-  nextArrow: '<span class="next_arrow"><i class="fas fa-angle-right"></i></span>',
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
+dots: false,
+infinite: false,
+speed: 300,
+slidesToShow: 3,
+slidesToScroll: 1,
+prevArrow: '<span class="prev_arrow"><i class="fas fa-angle-left"></i></span>',
+nextArrow: '<span class="next_arrow"><i class="fas fa-angle-right"></i></span>',
+responsive: [
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: false
     }
-  ]
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }
+]
 });
 // infinite scroll slide
 $('.infinite-slider').slick({
+dots: false,
+infinite: true,
+speed: 300,
+slidesToShow: 3,
+slidesToScroll: 1,
+prevArrow: '<span class="prev_arrow"><i class="fas fa-angle-left"></i></span>',
+nextArrow: '<span class="next_arrow"><i class="fas fa-angle-right"></i></span>',
+responsive: [
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: false
+    }
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1.2,
+      slidesToScroll: 1,
+      infinite: false,
+    }
+  }
+]
+});
+// alternate arrow
+$('.scroll').slick({
   dots: false,
-  infinite: true,
+  infinite: false,
   speed: 300,
   slidesToShow: 3,
   slidesToScroll: 1,
-  prevArrow: '<span class="prev_arrow"><i class="fas fa-angle-left"></i></span>',
-  nextArrow: '<span class="next_arrow"><i class="fas fa-angle-right"></i></span>',
+  prevArrow: '<span class="prev_arrow"><i class="fas fa-chevron-left fa-2x"></i></span>',
+  nextArrow: '<span class="next_arrow"><i class="fas fa-chevron-right fa-2x"></i></span>',
   responsive: [
     {
       breakpoint: 1024,
@@ -98,88 +134,54 @@ $('.infinite-slider').slick({
       breakpoint: 480,
       settings: {
         slidesToShow: 1.2,
-        slidesToScroll: 1,
-        infinite: false,
+        slidesToScroll: 1
       }
     }
   ]
 });
-// alternate arrow
-  $('.scroll').slick({
-    dots: false,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    prevArrow: '<span class="prev_arrow"><i class="fas fa-chevron-left fa-2x"></i></span>',
-    nextArrow: '<span class="next_arrow"><i class="fas fa-chevron-right fa-2x"></i></span>',
-    responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: false
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1.2,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
 // slider-one
 $('.slider-one').slick({
-  dots: false,
-  infinite: false,
-  speed: 300,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  prevArrow: '<span class="prev_arrow"><i class="fas fa-angle-left"></i></span>',
-  nextArrow: '<span class="next_arrow"><i class="fas fa-angle-right"></i></span>',
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: false
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-        slidesToShow: 1.2,
-        slidesToScroll: 1
-      }
+dots: false,
+infinite: false,
+speed: 300,
+slidesToShow: 1,
+slidesToScroll: 1,
+prevArrow: '<span class="prev_arrow"><i class="fas fa-angle-left"></i></span>',
+nextArrow: '<span class="next_arrow"><i class="fas fa-angle-right"></i></span>',
+responsive: [
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+      dots: false
     }
-  ]
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1.2,
+      slidesToScroll: 1
+    }
+  }
+]
 });
-// slide comp
+
+//slide-comp 
 
 $('.slide-comp').slick({
-  infinite: true,
-  speed: 300,
-  slidesToShow: 1,
-  prevArrow: '<span class="prev_arrow"><i class="fas fa-chevron-left fa-2x"></i></span>',
-  nextArrow: '<span class="next_arrow"><i class="fas fa-chevron-right fa-2x"></i></span>',
+infinite: true,
+speed: 300,
+slidesToShow: 1,
+prevArrow: '<span class="prev_arrow"><i class="fas fa-chevron-left fa-2x"></i></span>',
+nextArrow: '<span class="next_arrow"><i class="fas fa-chevron-right fa-2x"></i></span>',
 });
+
